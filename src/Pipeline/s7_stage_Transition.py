887c0d2f27ff2b5159687a27091b527f3f946ad6
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2025-04-13 23:11:44 +0530

    EC2 ADDED

--- src/Pipeline/s7_stage_Transition.py
+++ src/Pipeline/s7_stage_Transition.py
@@ -14,14 +14,7 @@ class ModelManager:
         self.info_path = info_path
         self.client = mlflow.tracking.MlflowClient()
         
-        dagshub_token = os.getenv("DAGSHUB_PAT")
-        if not dagshub_token:
-            raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
-        os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
-
-        mlflow.set_tracking_uri("https://dagshub.com/SHIVRAJSHINDE/AirlineFare_EndToEnd_02.mlflow")        
 
         # mlflow.set_tracking_uri("https://dagshub.com/SHIVRAJSHINDE/AirlineFare_EndToEnd.mlflow")
         # dagshub.init(repo_owner='SHIVRAJSHINDE', repo_name='AirlineFare_EndToEnd', mlflow=True)
@@ -74,6 +67,16 @@ class ModelManager:
 
 
 if __name__ == '__main__':
+    dagshub_token = os.getenv("DAGSHUB_PAT")
+    if not dagshub_token:
+        raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+    mlflow.set_tracking_uri("https://dagshub.com/SHIVRAJSHINDE/AirlineFare_EndToEnd_02.mlflow")        
+
+
     model_name="Lasso"
     info_path='reports/experiment_info.json'
 
